63b14c8bcd577ace756b77797426df6a5309122f
aouhman <abdessamad.ouhman@gmail.com>
2019-04-27 14:05:26 +0100

    generate new bundle  backend cmd php bin/console generate:bundle --namespace=BackendBundle --format=yml
    generate les entity cmd
     php bin/console doctrine:mapping:import BackendBundle yml

--- symfony/src/AppBundle/Controller/DefaultController.php
+++ symfony/src/AppBundle/Controller/DefaultController.php
@@ -8,9 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
+
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
@@ -18,13 +16,8 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
-    /**
-     * @Route("/pruebas", name="pruebas")
-     */
     public function pruebasAction()
     {
 
-        echo  "ABDESSAMAD " ;
-        die;
     }
 }
